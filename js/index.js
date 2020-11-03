442d3ad9bf1c662c4631434398c700212747299d
flpalacios93@gmail.comgit config --global user.name flap93git config --global user.name flpalacios93@gmail.com <flpalacios93@gmail.com>
2020-11-02 22:41:47 -0500

    progress

--- js/index.js
+++ js/index.js
@@ -2,22 +2,43 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  
 
-  //... your code goes here
+  const price = product.querySelector('.price span').innerHTML;
+  const quantity = product.querySelector('.quantity input').value;
+  const getSubtotal = price * quantity;
+  const subtotal = product.querySelector('.subtotal span')
+  return subtotal.innerHTML = getSubtotal;
+  console.log({getSubtotal});
+  return Number(getSubtotal);
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
+  
+  
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const products = document.querySelectorAll('.product');
+  productsCost = 0;
+
+  products.forEach(product => {
+    productsCost += updateSubtotal(product);
+
+  });
+
+
+
 
   // ITERATION 3
-  //... your code goes here
+
+  const totalInv = document.getElementById('total-value');
+  return totalInv.innerHTML = `$${productsCost.toFixed(2)}`;
+
 }
 
 // ITERATION 4
@@ -25,8 +46,10 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  
 }
+
+const remButton = document.getElementById('btn btn-remove');
 
 // ITERATION 5
 
